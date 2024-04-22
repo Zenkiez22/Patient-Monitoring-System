54b4dbec751078221389feaa4108db41003f204e
Zenkiez22 <96461012+Zenkiez22@users.noreply.github.com>
2024-04-22 17:37:07 +0800

    creating a base

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+</div>
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto font-weight-bolder text-primary">
+            <span>Copyright &copy; Patient Record Keeping with Inventory of Medicines 2024</span>
+            <div class="float-right d-sm-block font-italic text-danger">
+                - Kimberly Dela Cruz Duarte Group - CPMS Version 1.0
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
+
+</html>
+
+</html>
---
+++ includes/site_css_links.php
@@ -0,0 +1,18 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+
+<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+<!-- Theme style -->
+
+
+<link rel="stylesheet" href="dist/css/adminlte.min.css">
+
+<link rel="stylesheet" href="dist/js/jquery_confirm/jquery-confirm.css">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<link rel="stylesheet" href="dist/css/default.css" />
